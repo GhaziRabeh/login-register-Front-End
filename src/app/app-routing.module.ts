a2945a3fbb199694a616e5207192b4579f14daf6
Ghazi Rabeh <ghazirabeh9@gmail.com>
2024-11-26 14:10:22 +0100

    front-end

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { ListusersComponent } from './listeusers/listeusers.component';
+import { UpdateUserComponent } from './update-user/update-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'register', component:RegisterComponent},
+  {path : 'login' , component:LoginComponent},
+  {path : 'listusers' , component:ListusersComponent},
+  {path : 'users/update/:id' , component:UpdateUserComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
+import { ListusersComponent } from './listeusers/listeusers.component';
+import { UpdateUserComponent } from './update-user/update-user.component';
+import { ReactiveFormsModule } from '@angular/forms'; // Import this module
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    ListusersComponent,
+    UpdateUserComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
